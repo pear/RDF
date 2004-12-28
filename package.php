2988809c1b4e519d9fb922c62df5df81cd08d1b4
Lukas Smith <lsmith@php.net>
2004-12-28 17:43:30 +0000

    - update changelog
    git-svn-id: http://svn.php.net/repository/pear/packages/RDF/trunk@175811 c90b9560-bf6c-de11-be94-00142212c4b1

--- package.php
+++ package.php
@@ -2,9 +2,11 @@
 
 require_once 'PEAR/PackageFileManager.php';
 
-$version = '0.1.0alpha1';
+$version = '0.2.0';
 $notes = <<<EOT
-- initial release
+- fixed bug #3031 (Cannot re-assign this)
+- cosmetic fixes
+- propagate PEAR errors to the user
 EOT;
 
 $description =<<<EOT
@@ -12,7 +14,7 @@ This package is a port of the core components of the RDF API for PHP (aka RAP):
 http://www.wiwiss.fu-berlin.de/suhl/bizer/rdfapi/.
 EOT;
 
-$package = new PEAR_PackageFileManager();
+$package =& new PEAR_PackageFileManager();
 
 $result = $package->setOptions(array(
     'package'           => 'RDF',
